e557a56f56014683a24cedcd805d06736b9997f9
Mapelujo <olamideiyanda18@gmail.com>
2024-07-14 23:36:46 +0100

    progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import iconCheck from './images/icon-check.svg';
+import iconSun from './images/icon-sun.svg';
+import iconMoon from './images/icon-moon.svg';
+import iconCross from './images/icon-cross.svg';
+
 export default function App() {
   return (
     <div className="flex flex-col h-screen font-josefin">
-      <article className="border border-red-950 basis-[40%]">TOP</article>
-      <article className="border border-blue-950 basis-[60%]">BOTTOM</article>
+      <article className="basis-[40%] bg-desktop-dark bg-cover">TOP</article>
+      <article className="basis-[60%] bg-very-dark-blue">BOTTOM</article>
+
+      <main className="border border-white h-2/5 w-1/3 absolute top-[20%] left-1/2 -translate-x-1/2">
+        <div className='flex items-center justify-between'>
+          <h1 className='text-[2rem] text-light-grayish-blue tracking-widest font-bold'>TODO</h1>
+          <img className='block' src={iconSun} alt="sun"></img>
+        </div>
+      </main>
     </div>
   );
 }
