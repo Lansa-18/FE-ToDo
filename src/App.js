0d664354f69833a43fee992b250fda0bfc2ff481
Mapelujo <olamideiyanda18@gmail.com>
2024-07-16 18:52:30 +0100

    progress

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
+import { useState, useRef } from "react";
+import { useKey } from "./useKey";
 import iconCheck from "./images/icon-check.svg";
 import iconSun from "./images/icon-sun.svg";
 import iconMoon from "./images/icon-moon.svg";
 import iconCross from "./images/icon-cross.svg";
 
-const toDosObj = [
-  {
-    id: 1,
-    text: "Complete online JavaScript course",
-    completed: false,
-  },
-  {
-    id: 2,
-    text: "Jog around the park 3x",
-    completed: false,
-  },
-  {
-    id: 3,
-    text: "10 minutes meditation",
-    completed: false,
-  },
-  {
-    id: 4,
-    text: "Read for 1 hour",
-    completed: false,
-  },
-  {
-    id: 5,
-    text: "Pick up groceries",
-    completed: false,
-  },
-  {
-    id: 6,
-    text: "Complete Todo App on Frontend Mentor",
-    completed: false,
-  },
-];
-
-
 export default function App() {
+  const [inpValue, setInpValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const inputEl = useRef(null);
+
+  function onHandleAddTodo() {
+    setTodoList((todoArr) => [
+      ...todoArr,
+      {
+        text: inpValue,
+        completed: isCompleted,
+      },
+    ]);
+    setInpValue("");
+    inputEl.current.focus();
+  }
+
+  useKey("Enter", onHandleAddTodo);
+
   return (
     <div className="flex flex-col h-screen font-josefin">
       <article className="basis-[40%] bg-desktop-dark bg-cover"></article>
@@ -51,19 +39,30 @@ export default function App() {
           <img className="block" src={iconSun} alt="sun"></img>
         </div>
 
-        <Input />
-        <ToDoItems />
+        <Input
+          inputEl = {inputEl}
+          onAddTodo={onHandleAddTodo}
+          inpValue={inpValue}
+          onSetValue={setInpValue}
+        />
+        <ToDoItems todoList={todoList} />
       </main>
     </div>
   );
 }
 
-function Input() {
+function Input({ inpValue, onSetValue, onAddTodo, inputEl }) {
   return (
     <div className="bg-very-dark-desaturated-blue py-5 px-5 rounded-lg flex justify-between items-center mb-6">
-      <button className="border border-very-dark-grayish-blue rounded-full h-6 w-6"></button>
+      <button
+        className="border border-very-dark-grayish-blue rounded-full h-6 w-6"
+        onClick={onAddTodo}
+      ></button>
       <input
+        ref={inputEl}
         type="text"
+        value={inpValue}
+        onChange={(e) => onSetValue(e.target.value)}
         placeholder="Create a new todo..."
         className="w-[90%] border border-none outline-none bg-inherit text-light-grayish-blue placeholder-light-grayish-blue placeholder-opacity-50"
       ></input>
@@ -71,32 +70,32 @@ function Input() {
   );
 }
 
-function ToDoItems() {
+function ToDoItems({ todoList }) {
   return (
     <>
-    {toDosObj.map(todo => <ToDo key={todo.id} text={todo.text} />)}
-    <section className="bg-very-dark-desaturated-blue flex justify-between items-center text-[.8rem] text-dark-grayish-blue py-4 px-5">
-      <p>X items left</p>
-      <div className="flex justify-between w-1/3">
-        <p>All</p>
-        <p>Active</p>
-        <p>Completed</p>
-      </div>
-      <p>Clear Completed</p>
-    </section>
+      {todoList?.map((todo) => (
+        <ToDo key={todo.text} text={todo.text} />
+      ))}
+      <section className="bg-very-dark-desaturated-blue flex justify-between items-center text-[.8rem] text-dark-grayish-blue py-4 px-5">
+        <p className="cursor-pointer">X items left</p>
+        <div className="flex justify-between w-1/3">
+          <p className="cursor-pointer">All</p>
+          <p className="cursor-pointer">Active</p>
+          <p className="cursor-pointer">Completed</p>
+        </div>
+        <p className="cursor-pointer">Clear Completed</p>
+      </section>
     </>
   );
 }
 
 function ToDo({ text }) {
   return (
-      <div className="border-b-[1px] border-very-dark-grayish-blue bg-very-dark-desaturated-blue py-5 px-5 flex justify-between items-center cursor-pointer">
-        <button className="border border-very-dark-grayish-blue rounded-full h-6 w-6"></button>
-        <div
-          className="w-[90%] border border-none outline-none bg-inherit text-light-grayish-blue placeholder-light-grayish-blue placeholder-opacity-50"
-        >
-          {text}
-        </div>
+    <div className="border-b-[1px] border-very-dark-grayish-blue bg-very-dark-desaturated-blue py-5 px-5 flex justify-between items-center cursor-pointer">
+      <button className="border border-very-dark-grayish-blue rounded-full h-6 w-6"></button>
+      <div className="w-[90%] border border-none outline-none bg-inherit text-light-grayish-blue placeholder-light-grayish-blue placeholder-opacity-50">
+        {text}
       </div>
+    </div>
   );
 }
